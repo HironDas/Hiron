2c99425bfc11ed9efb4b9036e4d893a19b46f48d
hirondas <hiron_cse@yahoo.com>
2015-08-18 21:06:31 +0000

    the files are edited

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-//problem:
-//solution:
+//problem: we need a simple way to took at a user's badge count and javascript point from a web bowser
+//solution: Use Node.js to perform the profile look ups and server our  template via HTTP
+
+//1. Create a webserver
+//2. Handle HTTP route GET/ and POST
+//3. Handle http route get/:username i.e. /chalkers
+//4. Functio that handles the reading of file and merge in value
+
+
